4edba51ebd1093076db08b34f5e0636c9d9ff4da
boboiboyapi1 <boiboiboyapi90@gmail.com>
2023-12-17 01:11:35 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu')
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active');
+    menu.classList.toggle('menu-active');
+});
